67c38208d0d50174a4bb7d704a157c58a59b8171
Joseph Jeremiah <jerrycul2001@gmail.com>
2022-01-23 16:23:35 +0100

    added remote database connection

--- src/db.ts
+++ src/db.ts
@@ -1,12 +1,23 @@
 import {Client} from 'pg';
 
-const client = new Client({
-  user: process.env.PGUSER || 'localhost',
-  host: process.env.PGHOST || 'localhost',
-  database: process.env.PGDATABASE || 'artpage',
-  password: process.env.PGPASS || 'localhost',
-  port: Number(process.env.PGPORT) || 5432,
-});
+let client: Client;
+
+if (process.env.DATABSE_URL) {
+  client = new Client({
+    connectionString: process.env.DATABSE_URL,
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  });
+} else {
+  client = new Client({
+    user: process.env.PGUSER || 'localhost',
+    host: process.env.PGHOST || 'localhost',
+    database: process.env.PGDATABASE || 'artpage',
+    password: process.env.PGPASS || 'localhost',
+    port: Number(process.env.PGPORT) || 5432,
+  });
+}
 
 client.connect();
 // .then(() => console.log('Connected'))
