44c6fef5f0517e4ecc66661f5749b2e6ff49ef23
Joseph Jeremiah <jerrycul2001@gmail.com>
2022-01-24 09:09:02 +0100

    trying to fix origin issues

--- src/index.ts
+++ src/index.ts
@@ -16,6 +16,7 @@ const whitelist = [
 const corsOptions = {
   origin: function (origin: any, callback: any) {
     if (whitelist.indexOf(origin) !== -1) {
+      console.log(origin);
       callback(null, true);
     } else {
       callback(new Error(`${origin} not allowed by CORS`));
